0c0b8b54600fe0403da8e240e2eaf9711727a9af
sanskrutisawant2006-pixel <sanskrutisawant2006@gmail.com>
2026-05-05 15:43:41 -0700

    Update app.py

--- app.py
+++ app.py
@@ -84,4 +84,4 @@ def download():
 # 🔴 CRITICAL FIX FOR RENDER
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port))
+    app.run(host="0.0.0.0", port=port)
